622a4e698c3774050738ab6264a4f94f8a6f30dc
ikaroskappler <ikaros.kappler@gmx.de>
2019-12-09 22:45:03 +0100

    Publish script test ...

--- publish.sh
+++ publish.sh
@@ -1,8 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Compile ..."
 npm run webpack
 [ $? -eq 0 ]  || exit 1
 
+
+if [[ `git status --porcelain --untracked-files=no` ]]; then
+    echo "It seems your local git repository has uncommited changes. Please commit them before publishing."
+    exit 1
+else
+  echo "Your local git repository looks clean."
+fi
+
+
 echo "Publish ..."
-npm publish
+# npm publish
