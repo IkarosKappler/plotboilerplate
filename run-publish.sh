ce15b28473faab1c38875aada1fc6ea6ba91582e
ikaroskappler <ikaros.kappler@gmx.de>
2020-05-20 22:28:11 +0200

    Script tweak for version compare.

--- run-publish.sh
+++ run-publish.sh
@@ -24,12 +24,56 @@ PUBLIC_VERSION=$(npm view plotboilerplate version)
 LOCAL_VERSION=$(./run-get-package-version.sh)
 echo "PUBLIC_VERSION: $PUBLIC_VERSION"
 echo "LOCAL_VERSION: $LOCAL_VERSION"
+
+# Found at
+#   https://stackoverflow.com/questions/4023830/how-to-compare-two-strings-in-dot-separated-version-format-in-bash
+vercomp () {
+    if [[ $1 == $2 ]]
+    then
+        return 0
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 2
+        fi
+    done
+    return 0
+}
+
+
 if [ "$PUBLIC_VERSION" == "$LOCAL_VERSION" ]; then
     echo -e "${_RED}Cannot publish version. Local and public version are equal.${_NC}"
     exit 1
-elif [ "$PUBLIC_VERSION" \> "$LOCAL_VERSION" ]; then
-    echo -e "${_RED}Cannot publish version. Local version is lower than public version.${_NC}"
-    exit 1
+else
+    vercomp "$PUBLIC_VERSION" "$LOCAL_VERSION"
+    case $? in
+        0) op='=';;
+        1) op='>';;
+        2) op='<';;
+    esac
+    if [[ $op = ">" ]]
+    then
+	echo -e "${_RED}Cannot publish version. Local version is lower than public version.${_NC}"
+	exit 1
+    fi
 fi
 
 ./run-build.sh
@@ -41,7 +85,7 @@ git commit -m "Automatic commit."
 
 
 if [[ `git status --porcelain --untracked-files=no` ]]; then
-    echo "It seems your local git repository has uncommited changes. Please commit them before publishing."
+    echo -e "${_RED}It seems your local git repository has uncommited changes. Please commit them before publishing.${_NC}"
     exit 1
 else
   echo "Your local git repository looks clean."
@@ -57,7 +101,7 @@ echo "${_YELLOW}Exiting here ... TEST only${_NC}"
 exit 2
 
 echo "Publish ..."
-cd "./npm-package" && npm publish
+# cd "./npm-package" && npm publish
 
 if [ $? -eq 0 ]; then
     echo ""
