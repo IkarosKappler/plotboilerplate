e7d6f8c711221dd2422b2ea11c131bfb008d8b2c
ikaroskappler <ikaros.kappler@gmx.de>
2020-08-17 23:57:10 +0200

    Porting delaunay2voronoi from vanilla JS to TypeScript.

---
+++ src/js/utils/algorithms/delaunay2voronoi.d.ts
@@ -0,0 +1,21 @@
+/**
+ * Create the voronoi diagram from the given delaunay triangulation (they are dual graphs).
+ *
+ * @requires VoronoiCell, Triangle
+ *
+ * @author   Ikaros Kappler
+ * @date     2018-04-07
+ * @modified 2018-04-11 Using VoronoiCells now (was array before).
+ * @modified 2020-08-15 Ported from vanilla JS to TypeScript.
+ * @version  1.0.2
+ **/
+import { Triangle } from "../../Triangle";
+export declare class delaunay2voronoi {
+    failedTriangleSets: Array<Triangle>;
+    hasErrors: boolean;
+    private pointList;
+    private triangles;
+    constructor(pointList: any, triangles: any);
+    build(): any[];
+    subsetToPath(triangleSet: any, startPosition?: any, tryOnce?: any): any;
+}
