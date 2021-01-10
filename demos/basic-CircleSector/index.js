dc3ea42c3ab8b595ebc1119c03c9c2c4dc1f93f1
ikaroskappler <ikaros.kappler@gmx.de>
2021-01-10 19:30:41 +0100

    Added the missing CirclSector demo.

---
+++ demos/basic-CircleSector/index.js
@@ -0,0 +1,49 @@
+/**
+ * A script for demonstrating the basic usage of the Circle class.
+ *
+ * @requires PlotBoilerplate, gup, dat.gui, 
+ * 
+ * @author   Ikaros Kappler
+ * @date     2020-05-18
+ * @version  1.0.0
+ **/
+
+
+(function(_context) {
+    "use strict";
+
+    // Fetch the GET params
+    let GUP = gup();
+
+    window.addEventListener(
+	'load',
+	function() {
+	    // All config params except the canvas are optional.
+	    var pb = new PlotBoilerplate(
+		PlotBoilerplate.utils.safeMergeByKeys(
+		    { canvas                : document.getElementById('my-canvas'),
+		      fullSize              : true
+		    }, GUP
+		)
+	    );
+
+	    // Create center vertex and radius (a non-negative number)
+	    var center = new Vertex( 10, 10 );
+	    var radius = 150;
+
+	    // Create the circle
+	    var circle = new Circle( center, radius );
+
+	    // Now create a sector from the circle
+	    var startAngle   = Math.PI*0.3; // in radians
+	    var endAngle     = Math.PI*1.6;
+	    var circleSector = new CircleSector( circle, startAngle, endAngle );
+
+	    // Now add the sector to your canvas
+	    pb.add( circleSector );
+
+	    // Note: the center point is draggable now :)
+
+	} );
+    
+})(window); 
