385e6357c68a46c9d9e0cbc8572a8cc2c4d057d3
ikaroskappler <ikaros.kappler@gmx.de>
2021-02-23 23:42:37 +0100

    v1.12.0

--- bin/run-build.sh
+++ bin/run-build.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 
-# ./run-compile-typescript.sh
-cd .. && npm run compile-typescript-browser
+# cd .. && npm run compile-typescript-browser
+cd .. && npm run build
 
 [ $? -eq 0 ]  || exit 1
 
 cd bin
 
-echo "Running webpack-dev ..."
-npx webpack --config ./webpack5-dev.config.js
+# echo "Running webpack-dev ..."
+# npx webpack --config ./webpack5-dev.config.js
 
-echo "Running webpack-prod (browser) ..."
-npx webpack --config ./webpack5.browser.config.js
+# echo "Running webpack-prod (browser) ..."
+# npx webpack --config ./webpack5.browser.config.js
 
-# echo "Running webpack-prod (module) ..."
-# npx webpack --config ./webpack5.module.config.js
+# cd .. && npm run compile-typescript-module
 
-cd .. && npm run compile-typescript-module
+# echo "Running rollup (module) ..."
+# npm run rollup-module
 
-echo "Running rollup (module) ..."
-npm run rollup-module
+
