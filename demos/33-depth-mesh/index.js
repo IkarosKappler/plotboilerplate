d5412520af85f7dd5a028deb3476dff1bae0fe68
Ikaros Kappler <info@int2byte.de>
2021-03-31 23:34:10 +0200

    Tweaking the depth mesh demo (sphere).

--- demos/33-depth-mesh/index.js
+++ demos/33-depth-mesh/index.js
@@ -66,33 +66,33 @@
     };
 
     var makeSphereGeometry = function () {
-      var longs = 6;
+      var longs = 7;
       var lats = 12;
       var radius = 1;
       var vertices = [];
       var edges = [];
       for (var long = 0; long < longs; long++) {
-        var theta = Math.PI / 2 + (long / longs) * Math.PI;
+        var theta = Math.PI / 2 + (long / (longs - 1)) * Math.PI;
         for (var lat = 0; lat < lats; lat++) {
           var alpha = (lat / lats) * Math.PI * 2;
-          // var x = x0 + radius * Math.cos(theta) * Math.sin(alpha);
-          // var y = y0 + radius * Math.sin(theta) * Math.sin(alpha);
-          // var z = z0 + radius * Math.cos(alpha);
-          // var vert = new Vert3(x, y, z);
           var vert = new Vert3(radius, 0, 0);
           vert = rotatePoint(vert, theta, 0, alpha, 0);
           vertices.push(vert);
-          // if (lat > 0 && long > 0) {
           if (lat > 0) {
+            // Make longitudes
             edges.push([long * lats + lat - 1, long * lats + lat]);
-            if (lat + 1 == lats) {
+            if (lat + 1 === lats) {
               edges.push([long * lats, long * lats + lat]);
             }
-            // edges.push([lat * longs + long - 1, lat * longs + long - 1]);
+            // Make latitudes
             if (long > 0) {
-              // && long + 1 < longs) {
               edges.push([long * lats + lat - 1, (long - 1) * lats + lat - 1]);
+              if (long + 1 === longs) {
+                //edges.push([long * lats, (long - 1) * lats + lat - 1]);
+              }
             }
+          } else if (long > 0) {
+            edges.push([long * lats + lats - 1, (long - 1) * lats + lats - 1]);
           }
         }
       }
