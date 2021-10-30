44ff54dc96b4c466ba6bdd7f4ac1d2dbe57c0a54
Ikaros Kappler <info@int2byte.de>
2021-10-30 12:58:09 +0200

    Adding iterations and life cycles.

--- demos/37-conways-game-of-life/index.js
+++ demos/37-conways-game-of-life/index.js
@@ -1,7 +1,5 @@
 /**
- * A demo to visualize the 'lightning algorithm' as depicted in Numberphile's
- * and Matt Henderson's video:
- * https://www.youtube.com/watch?v=akZ8JJ4gGLs
+ * A demo to visualize 'Conway's Game Of Life'.
  *
  * @requires PlotBoilerplate
  * @requires MouseHandler
@@ -14,8 +12,6 @@
  * @date        2021-10-04
  * @version     1.0.0
  **/
-
-const { getCombinedNodeFlags } = require("typescript");
 
 (function (_context) {
   "use strict";
@@ -35,30 +31,36 @@ const { getCombinedNodeFlags } = require("typescript");
       {
         animate: true,
         animationDelay: 150,
-        
+
         cellWidth: 32,
         cellHeight: 32,
         borderSize: 2,
 
-        // mazeWidth: 60,
-        // mazeHeight: 40,
+        // randomizeBiome: false,
+        randomizationThreshold: 0.7,
 
-        drawPathTraces: true,
-        wallColor: "#880088",
-        deadEndWallColor: "#b84800",
-        traceColor: "#0088a8",
-        lightningColor: "#ffffff",
-
-        falloff: 1,
-
-        reset: function () {
-          config.animate = false;
-          removeTraces();
-          initQueue();
-          visualizeSolution();
+        preset_glider: function () {
+          addGliderAt(0, 0);
+          visualizeCreatures();
         },
-        rebuildMaze: function () {
-          rebuildBiotope();
+
+        clear: function () {
+          rebuildBiotope(false);
+          // if (!config.randomizeBiome) {
+          //   initCreatures();
+          // }
+          visualizeCreatures();
+        },
+        randomize: function () {
+          rebuildBiotope(true);
+          // if (!config.randomizeBiome) {
+          //   initCreatures();
+          // }
+          visualizeCreatures();
+        },
+        nextStep: function () {
+          nextStep();
+          visualizeCreatures();
         }
       },
       GUP
@@ -72,7 +74,7 @@ const { getCombinedNodeFlags } = require("typescript");
     // var squareSize = null;
     var tosvgDraw = null;
     var tosvgFill = null;
-    var biotopeSize = { w : 0, h : 0 };
+    var biotopeSize = { width: 0, height: 0 }; // Dimension
 
     // +---------------------------------------------------------------------------------
     // | Initialize the draw library for SVG rendering: width and height and style defs.
@@ -88,34 +90,27 @@ const { getCombinedNodeFlags } = require("typescript");
         drawConfig // see file drawConfig.js
       );
       tosvgFill = tosvgDraw.copyInstance(true); // fillShapes=true
-      // squareSize = {
-      //   w: canvasSize.width / config.mazeWidth,
-      //   h: canvasSize.height / config.mazeHeight
-      // };
-      // var innerSquareSize = {
-      //   w: squareSize.w - config.borderSize,
-      //   h: squareSize.h - config.borderSize
-      // };
-      biotopeSize.width = Math.floor( canvasSize.width / config.cellWidth );
-      biotopeSize.height = Math.floor( canvasSize.height / config.cellHeight );
+      biotopeSize.width = Math.floor(canvasSize.width / config.cellWidth);
+      biotopeSize.height = Math.floor(canvasSize.height / config.cellHeight);
       // var styleDefs = makeCustomStyleDefs(config, innerSquareSize);
       // tosvgDraw.addCustomStyleDefs(styleDefs);
     };
 
-    // Array<number[]>
-    var biotope = [];
+    // Array<boolean[]>
+    var biome = [];
+    var stepNumber = 0;
 
     // +---------------------------------------------------------------------------------
     // | This function rebuilds the maze and renders it onto the SVG canvas.
     // | The old SVG data is cleared.
     // +-------------------------------
-    var rebuildBiotope = function () {
+    var rebuildBiotope = function (randomizeBiome) {
       config.animate = false;
       // Just remove all child nodes from the SVG an re-initialize everything from scratch.
       removeAllChildNodes(svgNode);
       // This initializes the maze structure and draws the walls on the SVG canvas.
-      initBiotope();
-     
+      initBiotope(randomizeBiome);
+
       // TODO: initialize the creatures here
       // initCreatues();
     };
@@ -123,19 +118,19 @@ const { getCombinedNodeFlags } = require("typescript");
     // +---------------------------------------------------------------------------------
     // | Ths function computes the biotope and renders it.
     // +-------------------------------
-    var initBiotope = function () {
+    var initBiotope = function (randomizeBiome) {
       initSvgCanvas();
       tosvgDraw.beginDrawCycle();
       tosvgFill.beginDrawCycle();
       tosvgFill.curClassName = "background";
       tosvgFill.rect({ x: 0, y: 0 }, canvasSize.width, canvasSize.height, "#888888", 0);
-      biotope = [];
+      biome = [];
       for (var j = 0; j < biotopeSize.height; j++) {
-        biotope.push([]);
+        var row = [];
+        biome.push(row);
         for (var i = 0; i < biotopeSize.width; i++) {
-
           // Make square and add respective classes to visualize the borders.
-          tosvgFill.curClassName = "cell"
+          tosvgFill.curClassName = "cell";
           tosvgFill.curId = "cell-" + j + "-" + i;
           tosvgFill.rect(
             { x: i * config.cellWidth, y: j * config.cellHeight },
@@ -144,30 +139,46 @@ const { getCombinedNodeFlags } = require("typescript");
             "#ffffff",
             1
           );
+
+          // Init with random pattern?
+          if (randomizeBiome) {
+            row.push(Math.random() < config.randomizationThreshold);
+          } else {
+            row.push(false);
+          }
         }
       }
       tosvgDraw.endDrawCycle();
       tosvgFill.endDrawCycle();
     };
 
-    var initCreatures = function ( ){
-      // var center = { x : Math.floor( biotopeSize.width/2 ) };
-    }
-
-    // interface MazeEntry {
-    //  i: number;
-    //  j: number;
-    // }
-
-    // Array<MazeEntry>
-    var queue = [];
-    // Array<Array<{ step: number, predecessor: undefined | MazeEntry }>>
-    var solutionMatrix = [];
-    // number
-    var stepNumber = 1;
-    // MazeEntry
-    var terminationEntry = null;
-
+    // +---------------------------------------------------------------------------------
+    // | This function rebuilds the maze and renders it onto the SVG canvas.
+    // | The old SVG data is cleared.
+    // +-------------------------------
+    var setCellAlive = function (position, alive) {
+      // console.log("setCellAlive", position);
+      if (position.i >= 0 && position.j >= 0 && position.j < biome.length && position.i < biome[position.j].length) {
+        biome[position.j][position.i] = alive;
+      }
+    };
+    var relCol = function (i) {
+      return i + Math.floor(biotopeSize.width / 2);
+    };
+    var relRow = function (j) {
+      return j + Math.floor(biotopeSize.height / 2);
+    };
+    var relPos = function (j, i) {
+      return { j: relRow(j), i: relCol(i) };
+    };
+    var addGliderAt = function (j, i) {
+      setCellAlive(relPos(j, i + 1), true);
+      setCellAlive(relPos(j + 1, i + 2), true);
+      setCellAlive(relPos(j + 2, i), true);
+      setCellAlive(relPos(j + 2, i + 1), true);
+      setCellAlive(relPos(j + 2, i + 2), true);
+      console.log("biome", biome);
+    };
 
     // +---------------------------------------------------------------------------------
     // | Visualize the algorithms (partial) solution inside the SVG.
@@ -175,33 +186,18 @@ const { getCombinedNodeFlags } = require("typescript");
     // | to indicate the current frontiers.
     // +-------------------------------
     var visualizeCreatures = function () {
-      for (var j = 0; j < solutionMatrix.length; j++) {
-        for (var i = 0; i < solutionMatrix[j].length; i++) {
+      for (var j = 0; j < biome.length; j++) {
+        for (var i = 0; i < biome[j].length; i++) {
           // var ratio = Math.max(0, solutionMatrix[j][i].step / stepNumber) * 0.5;
-          var ratio =
-            Math.max(0, Math.pow(solutionMatrix[j][i].step, config.falloff) / Math.pow(stepNumber, config.falloff)) * 0.5;
-          var rectangle = document.getElementById("rect-" + j + "-" + i);
-          rectangle.setAttribute("fill", "rgba(255,255,255," + ratio + ")");
+          var rectangle = document.getElementById("cell-" + j + "-" + i);
+          if (biome[j][i]) {
+            rectangle.setAttribute("fill", "rgba(0,0,0,1.0)");
+          } else {
+            rectangle.setAttribute("fill", "rgba(255,255,255,1.0)");
+          }
         }
       }
     };
-
-    // +---------------------------------------------------------------------------------
-    // | If a shortest path (the final solution) was found after the the algorithm
-    // | terminated then this function highlights that path by coloring the
-    // | affetced rectangles with a bright color.
-    // +-------------------------------
-    // var visualizeTrace = function () {
-    //   // Visualize solution
-    //   var entry = terminationEntry;
-    //   while (entry) {
-    //     var i = entry.i;
-    //     var j = entry.j;
-    //     var rectangle = document.getElementById("rect-" + j + "-" + i);
-    //     rectangle.setAttribute("fill", config.lightningColor);
-    //     entry = solutionMatrix[j][i].predecessor;
-    //   }
-    // };
 
     // +---------------------------------------------------------------------------------
     // | Calculates the next step of the BFS: all rectangles on the current frontier
@@ -209,44 +205,73 @@ const { getCombinedNodeFlags } = require("typescript");
     // | in the next iteration.
     // +-------------------------------
     var nextStep = function () {
-      // Calculate next iteration of the breadth-first-algorithm
+      // Calculate next iteration of the game
       stepNumber++;
-      
-      // TODO: evaluate
+
+      var newBiotope = [];
+      for (var j = 0; j < biome.length; j++) {
+        var row = []; // TODO: check if new Array(n) is better here (we know the size)
+        newBiotope.push(row);
+        for (var i = 0; i < biome[j].length; i++) {
+          var isAlive = biome[j][i];
+          var neighbourCount = getNumberOfLivingNeighbours(j, i);
+          if (isAlive) {
+            // console.log("Cell j=" + j + " i=" + i + " is alive and has " + neighbourCount + " neighbours");
+            if (neighbourCount < 2) {
+              // Die of under-population
+              row.push(false);
+            } else if (neighbourCount == 2 || neighbourCount == 3) {
+              // Keep on living
+              row.push(true);
+            } else if (neighbourCount > 3) {
+              // Die of under-population
+              row.push(false);
+            } else {
+              row.push(false);
+            }
+          } else {
+            if (neighbourCount === 3) {
+              // Dead cell becomes alive dues to 3 living neighboures
+              row.push(true);
+            } else {
+              // Dead cell stays dead due to under- or over-poulation
+              row.push(false);
+            }
+          }
+        }
+      } // END for
+      biome = newBiotope;
     };
 
-    // +---------------------------------------------------------------------------------
-    // | Visualize a single trace from one maze position to the next.
-    // | The function adds new <line> elements to the SVG!
-    // +-------------------------------
-    // var makeTrace = function (fromJ, fromI, toJ, toI) {
-    //   var fromRect = document.getElementById("rect-" + fromJ + "-" + fromI);
-    //   var toRect = document.getElementById("rect-" + toJ + "-" + toI);
-    //   var centerFrom = {
-    //     x: parseFloat(fromRect.getAttribute("x")) + parseFloat(fromRect.getAttribute("width")) / 2,
-    //     y: parseFloat(fromRect.getAttribute("y")) + parseFloat(fromRect.getAttribute("height")) / 2
-    //   };
-    //   var centerTo = {
-    //     x: parseFloat(toRect.getAttribute("x")) + parseFloat(toRect.getAttribute("width")) / 2,
-    //     y: parseFloat(toRect.getAttribute("y")) + parseFloat(toRect.getAttribute("height")) / 2
-    //   };
-
-    //   tosvgDraw.curClassName = "trace";
-    //   // Note that outside draw cycles this will have no effect!
-    //   // The elements are just stored inside the libraries internal buffer and would
-    //   // become visible on the next draw cycle ... but ... there is none: once
-    //   // the rectangles where drawn the draw cycle has ended.
-    //   var lineNode = tosvgDraw.line(centerFrom, centerTo, config.traceColor, 1.0);
-    //   svgNode.getElementsByTagName("g")[0].appendChild(lineNode);
-    // };
-
-
-    // +---------------------------------------------------------------------------------
-    // | Removed the traces withour clearing the whole SVG.
-    // +-------------------------------
-    // var removeTraces = function () {
-    //   removeChildNodesByClass(svgNode.getElementsByTagName("g")[0], "trace");
-    // };
+    var getNumberOfLivingNeighbours = function (j, i) {
+      var count = 0;
+      if (j - 1 >= 0 && biome[j - 1][i]) {
+        count++;
+      }
+      if (j + 1 < biome.length && biome[j + 1][i]) {
+        count++;
+      }
+      if (i - 1 >= 0 && biome[j][i - 1]) {
+        count++;
+      }
+      if (i + 1 < biome[j].length && biome[j][i + 1]) {
+        count++;
+      }
+      // Also look at diagoal neighbours
+      if (j - 1 >= 0 && i - 1 >= 0 && biome[j - 1][i - 1]) {
+        count++;
+      }
+      if (j + 1 < biome.length && i - 1 >= 0 && biome[j + 1][i - 1]) {
+        count++;
+      }
+      if (j + 1 < biome.length && i + 1 < biome[j].length && biome[j + 1][i + 1]) {
+        count++;
+      }
+      if (j - 1 >= 0 && i + 1 < biome[j].length && biome[j - 1][i + 1]) {
+        count++;
+      }
+      return count;
+    };
 
     // +---------------------------------------------------------------------------------
     // | This function is called on each frame draw.
@@ -270,8 +295,6 @@ const { getCombinedNodeFlags } = require("typescript");
       if (!config.animate) {
         return;
       }
-      // removeTraces();
-      // initializeCreatures();
       renderLoop();
     };
 
@@ -285,26 +308,34 @@ const { getCombinedNodeFlags } = require("typescript");
     f0.add(config, "animate").listen().title("Toggle animation on/off.").onChange(startAnimation);
     f0.add(config, "animationDelay").min(10).max(1000).title("The delay in milliseconds between frames.");
 
-    f0.add(config, "borderSize").min(0).max(10).step(1).title("borderSize").onChange(rebuildBiotope);
-    f0.add(config, "cellWidth").min(2).max(100).title("cellWidth").onChange(rebuildBiotope);
-    f0.add(config, "cellHeight").min(2).max(100).title("cellHeight").onChange(rebuildBiotope);
+    // prettier-ignore
+    f0.add(config, "borderSize").min(0).max(10).step(1).title("borderSize").onChange(function () {
+        rebuildBiotope(false);
+      });
+    // prettier-ignore
+    f0.add(config, "cellWidth").min(2).max(100).title("cellWidth").onChange(function () {
+        rebuildBiotope(false);
+      });
+    // prettier-ignore
+    f0.add(config, "cellHeight").min(2).max(100).title("cellHeight")
+      .onChange(function () {
+        rebuildBiotope(false);
+      });
 
-    f0.add(config, "drawPathTraces").listen().title("Draw path traces?");
-    // f0.addColor(config, "wallColor").title("The wall color.").onChange(rebuildBiotope);
-    // f0.addColor(config, "deadEndWallColor").title("The 'dead end' wall color.").onChange(rebuildBiotope);
-    // f0.addColor(config, "traceColor").title("The trace color.").onChange(rebuildBiotope);
-    // f0.addColor(config, "lightningColor").title("The lightning color.").onChange(visualizeTrace);
+    // prettier-ignore
+    f0.add(config, "randomizationThreshold").min(2).max(100).title("The probabily that a new cell is alive.");
 
-    f0.add(config, "falloff").min(1).max(10).step(0.25).title("falloff").onChange(visualizeCreatures);
+    var f1 = gui.addFolder("Biomes");
+    f1.add(config, "preset_glider");
+    f1.open();
 
-    f0.add(config, "reset");
-    f0.add(config, "rebuildMaze");
+    f0.add(config, "clear");
+    f0.add(config, "randomize");
+    f0.add(config, "nextStep");
 
     f0.open();
 
     initBiotope();
-    // printMazeMatrix(); // Used for debugging.
-    initCreatures();
     visualizeCreatures();
     startAnimation();
   });
