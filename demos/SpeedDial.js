a95cdecaa2f3c543f401edebd778fc0b8cd8a134
Ikaros Kappler <info@int2byte.de>
2021-12-09 21:54:21 +0100

    Tweaking mobile size and in-out transition.

--- demos/SpeedDial.js
+++ demos/SpeedDial.js
@@ -5,11 +5,16 @@
  */
 
 globalThis.SpeedDial = globalThis.SpeedDial || (function() {
-    
+
     var BUTTON_SIZE = 40;
     var GAP_SIZE = 4;
 
     var SD = function () {
+
+        var isMobile = typeof isMobileDevice === "function" && isMobileDevice();
+        this.buttonSize = isMobile ? BUTTON_SIZE*2 : BUTTON_SIZE;
+        this.gapSize = isMobile ? GAP_SIZE*2 : GAP_SIZE;
+
         this.rootNode = createRootNode(this);
         document.querySelector("body").appendChild( this.rootNode );
 
@@ -17,12 +22,13 @@ globalThis.SpeedDial = globalThis.SpeedDial || (function() {
     };
 
     SD.prototype.addActionButton = function( label, onClick ) {
-        var buttonNode = createButtonNode( onClick );
+        var buttonNode = createButtonNode( this, onClick );
         var offset = this._buttons.length+1;
-        buttonNode.style["transform"] = "translateY(-"+(offset*(BUTTON_SIZE+GAP_SIZE))+"px)";
+        buttonNode.style["transform"] = "translate(0px,0px)"; 
         buttonNode.style["opacity"] = "0.0";
-        buttonNode.style["transition"] = "opacity "+(offset*0.25)+"s";
+        buttonNode.style["transition"] = "opacity "+(offset*0.25)+"s, transform 0.5s";
         buttonNode.style["background-color"] = "rgb(206,0,192)";
+        buttonNode.style["z-index"] = (-offset); // Something smaller than 100
         buttonNode.innerHTML = label;
         this.rootNode.appendChild( buttonNode );
         this._buttons.push( buttonNode );
@@ -30,17 +36,21 @@ globalThis.SpeedDial = globalThis.SpeedDial || (function() {
 
     var showDial = function( dial ) {
         dial.rootNode.style["overflow"] = "visible";
-        dial.rootNode.style["height"] = ""+((dial._buttons.length+1)*(BUTTON_SIZE+GAP_SIZE))+"px";
-        dial._buttons.forEach( function(button) {
-            button.style["opacity"] = "1.0";
+        dial.rootNode.style["height"] = ""+((dial._buttons.length+1)*(dial.buttonSize+dial.gapSize))+"px";
+        dial._buttons.forEach( function(buttonNode,index) {
+            var offset = index+1;
+            buttonNode.style["opacity"] = "1.0";
+            buttonNode.style["transform"] = "translate(0px,-"+(offset*(dial.buttonSize+dial.gapSize))+"px)";
         });
     }
 
     var hideDial = function( dial ) {
-        dial.rootNode.style["overflow"] = "hidden";
-        dial.rootNode.style["height"] = ""+BUTTON_SIZE+"px";
-        dial._buttons.forEach( function(button) {
-            button.style["opacity"] = "0.0";
+        // Activate this line to fade-out immediately
+        // dial.rootNode.style["overflow"] = "hidden";
+        dial.rootNode.style["height"] = ""+dial.buttonSize+"px";
+        dial._buttons.forEach( function(buttonNode) {
+            buttonNode.style["opacity"] = "0.0";
+            buttonNode.style["transform"] = "translate(0px,0px)";
         });
     }
 
@@ -49,14 +59,15 @@ globalThis.SpeedDial = globalThis.SpeedDial || (function() {
         node.style["position"] = "absolute";
         node.style["bottom"] = "0px";
         node.style["left"] = "50vw";
-        node.style["transform"] = "translate(-50%)";
-        node.style["width"] = ""+BUTTON_SIZE+"px";
-        node.style["height"] = ""+BUTTON_SIZE+"px";
-        node.style["border-radius"] = "20px";
+        node.style["transform"] = "translate(-50%, 0px)";
+        node.style["width"] = ""+dial.buttonSize+"px";
+        node.style["height"] = ""+dial.buttonSize+"px";
+        node.style["border-radius"] = ""+(dial.buttonSize/2)+"px";
         node.style["border"] = "0";
         node.style["overflow"] = "hidden";
+        node.style["z-index"] = "100";
 
-        var buttonNode = createButtonNode(function() {
+        var buttonNode = createButtonNode( dial, function() {
             console.log("click");
         } );
 
@@ -74,15 +85,15 @@ globalThis.SpeedDial = globalThis.SpeedDial || (function() {
         return node;
     };
 
-    var createButtonNode = function( onClick ) {
+    var createButtonNode = function( dial, onClick ) {
         var buttonNode = document.createElement("button");
         buttonNode.style["position"] = "absolute";
         buttonNode.style["bottom"] = "0px";
-        buttonNode.style["width"] = ""+BUTTON_SIZE+"px";
-        buttonNode.style["height"] = ""+BUTTON_SIZE+"px";
+        buttonNode.style["width"] = ""+dial.buttonSize+"px";
+        buttonNode.style["height"] = ""+dial.buttonSize+"px";
         buttonNode.style["background-color"] = "rgb(0,128,192)";
         buttonNode.style["border-width"] = "0px";
-        buttonNode.style["border-radius"] = "20px";
+        buttonNode.style["border-radius"] = ""+(dial.buttonSize/2)+"px";
         buttonNode.style["outline"] = "none";
 
         buttonNode.addEventListener("click", onClick); 
