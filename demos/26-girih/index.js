fd7817e417fdee89f45e5a97bec4dc9a4400eed7
Ikaros Kappler <info@int2byte.de>
2022-07-29 23:33:35 +0200

    Fixing the event handling in the SVG demo.

--- demos/26-girih/index.js
+++ demos/26-girih/index.js
@@ -301,7 +301,7 @@
     // +---------------------------------------------------------------------------------
     // | Add a mouse listener to track the mouse position.
     // +-------------------------------
-    new MouseHandler(pb.canvas, "girih-demo")
+    new MouseHandler(pb.eventCatcher, "girih-demo")
       .move(function (e) {
         var relPos = pb.transformMousePosition(e.params.pos.x, e.params.pos.y);
         var cx = document.getElementById("cx");
