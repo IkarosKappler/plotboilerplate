d23af59874d662447d5068bcacefb8e62611445e
Ikaros Kappler <info@int2byte.de>
2024-07-24 00:49:35 +0200

    Upading the publish script; Jekyll is not needed any more.

--- bin/run-publish.sh
+++ bin/run-publish.sh
@@ -11,7 +11,7 @@ _NC='\033[0m'
 
 while true; do
     echo -e "${_PURPLE}Do you really want to publish the new version?${_NC}"
-    read -p "webpack and jekyll will be run again in this process. (y/n)? " yn
+    read -p "webpack will be run again in this process. (y/n)? " yn
     case $yn in
         [Yy]* ) echo "Failsafe."; break;;
         [Nn]* ) exit;;
